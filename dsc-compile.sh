f5274746f25e0bf420ae43e33d7d87d56bc26e2a
lorax79 <jordan.smith@powerhell.org>
2018-05-16 16:59:53 -0700

    curl commands added

--- dsc-compile.sh
+++ dsc-compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 DATA='{"automationRG":"AutomationAccounts","automationAcct":"PoshPug"}'
-URL=https://s1events.azure-automation.net/webhooks?token=f423P5jNHDo2mdDCKRCuUJcKy7srv8Z1q%2bJgfGOIggE%3d
+URL=https://s1events.azure-automation.net/webhooks?token=QYtoRTGQp8z0Fwm5pwGyMnSW56LQ%2b0uXm5CAqsfkjK0%3d
 
 curl -d $DATA $URL
--- vmcurl.sh
+++ vmcurl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DATA='{"vnetName":"vnet1","region":"westus2","vmPrefix":"demovm0","numberOfInstances":"2","dscNodeName":"HybridWorkerNode.Localhost"}'
-URL=https://s1events.azure-automation.net/webhooks?token=f423P5jNHDo2mdDCKRCuUJcKy7srv8Z1q%2bJgfGOIggE%3d
+DATA='{"vnetName":"vnet1","region":"westus2","vmPrefix":"demovm0","numberOfInstances":2,"dscNodeName":"HybridWorkerNode.Localhost"}'
+URL=https://s1events.azure-automation.net/webhooks?token=OWmtfCZFcM3DjudeTupkd3j%2fYM85jmJip5gP7C6D%2flE%3d
 
 curl -d $DATA $URL
