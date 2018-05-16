aedcb566e16a3840fc443de73ad2e267ee71bbae
Jordan Smith <jorsmith@microsoft.com>
2018-05-16 12:10:47 -0700

    hybrid worker onboarding

---
+++ runbooks/Compile-DSC.ps1
@@ -0,0 +1,45 @@
+﻿param(
+$automationRG,
+$automationAcct
+)
+
+    
+    $connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}            $ConfigurationData = @{
+        AllNodes = @(
+            @{
+                NodeName = 'Localhost'
+                PSDscAllowPlainTextPassword = $true
+            }
+        )
+    }
+
+    $CompileParams = @{
+        ResourceGroupName     = $automationRG
+        AutomationAccountName = $automationAcct
+        ConfigurationName     = HybridWorkerNode
+        ConfigurationData     = $ConfigurationData
+    }
+
+    Start-AzureRmAutomationDscCompilationJob @CompileParams
